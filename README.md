8c0c79c1c5ed6de3cfc6aebbe13d9f3236f50293
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-27 18:33:13 -0300

    inicio de portifólio

---
+++ README.md
@@ -0,0 +1,7 @@
+# protótipo para portifolio
+
+portifólio com ajax e bootstrap 4
+
+# referencias
+- https://www.youtube.com/watch?v=rJesac0_Ftw
+- https://getbootstrap.com/
---
+++ js/main.js
@@ -0,0 +1,30 @@
+var animalContainer = document.getElementById("projetos");
+var btn = document.getElementById("btn");
+var countProjetos = 0;
+
+btn.addEventListener("click",function(){
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET','./data.json');
+    ourRequest.onload = function(){
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+});
+
+function renderHTML(data){
+    var htmlString = "";
+    for (i = 0;i < data.length; i++){
+        htmlString += "<div class='col-sm-12 col-md-4'>" +
+        "<h3 class='text-center'>" + data[i].nome + "</h3>" +
+        "<p>" + data[i].imagem +"</p>" +
+        "<h4>Descrição</h4>" + 
+        "<p>" + data[i].descricao + "</p>" +
+        "<div class='btn-group'>" +
+            "<a href='" + data[i].link + "' class='btn btn-primary'>ver projeto</a>" +
+            "<a href='" + data[i].codigoFonte + "' class='btn btn-primary'>source</a>" +
+        "</div>" +   
+         "</div>";
+    }
+    animalContainer.insertAdjacentHTML('beforeend',htmlString);
+}
