a2ec40e25d5d495159c824de9a4ef53667636023
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-27 19:18:01 -0300

    add css

--- js/main.js
+++ js/main.js
@@ -15,14 +15,14 @@ btn.addEventListener("click",function(){
 function renderHTML(data){
     var htmlString = "";
     for (i = 0;i < data.length; i++){
-        htmlString += "<div class='col-sm-12 col-md-4'>" +
+        htmlString += "<div class='col-sm-12 col-md-4 bloco_projeto'>" +
         "<h3 class='text-center'>" + data[i].nome + "</h3>" +
         "<p>" + data[i].imagem +"</p>" +
         "<h4>Descrição</h4>" + 
         "<p>" + data[i].descricao + "</p>" +
-        "<div class='btn-group'>" +
-            "<a href='" + data[i].link + "' class='btn btn-primary'>ver projeto</a>" +
-            "<a href='" + data[i].codigoFonte + "' class='btn btn-primary'>source</a>" +
+        "<div class='btn-group '>" +
+            "<a href='" + data[i].link + "' class='btn btn-primary '>ver projeto</a>" +
+            "<a href='" + data[i].codigoFonte + "' class='btn btn-primary '>source</a>" +
         "</div>" +   
          "</div>";
     }
