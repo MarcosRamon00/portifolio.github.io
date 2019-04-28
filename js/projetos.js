38cc60c8255c88953efcd6ab5475b18bf9962163
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-28 02:05:35 -0300

    modificacao de index e css

--- js/projetos.js
+++ js/projetos.js
@@ -1,20 +1,15 @@
 var area_projetos = document.getElementById("area_projetos");
-var btn_projetos = document.getElementById("btn_projetos");
-var isBtnProjetosAtivo = true;
 
-btn_projetos.addEventListener("click",function(){
-    var request = new XMLHttpRequest();
-    request.open('GET','./json/projetos.json');
-    request.onload = function(){
-        var data = JSON.parse(request.responseText);
-        if(isBtnProjetosAtivo){
-            renderTodosProjetos(data);
-            isBtnProjetosAtivo = false;
-        }
-    };
-    request.send();
+
+var request = new XMLHttpRequest();
+request.open('GET','./json/projetos.json');
+request.onload = function(){
+    var data = JSON.parse(request.responseText);
+     renderTodosProjetos(data);
     
-});
+};
+request.send();
+    
 
 function renderTodosProjetos(data){
     var bloco_projeto = "";
@@ -25,8 +20,8 @@ function renderTodosProjetos(data){
         "<h4>Descrição</h4>" + 
         "<p>" + data[i].descricao + "</p>" +
         "<div class='btn-group btn-block'>" +
-            "<a href='" + data[i].link + "' class='btn btn-primary '>ver projeto</a>" +
-            "<a href='" + data[i].codigoFonte + "' class='btn btn-primary '>código fonte</a>" +
+            "<a href='" + data[i].link + "' class='btn btn-secondary '>ver projeto</a>" +
+            "<a href='" + data[i].codigoFonte + "' class='btn btn-secondary '>código fonte</a>" +
         "</div>" +   
          "</div>";
     }
