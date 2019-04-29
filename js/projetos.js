27a06d092866a7deba6e701d2baccacff10a5f29
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-29 12:44:17 -0300

    refatoracao de renderizarHTMLProjetos e opcao caso projeto nao possua links

--- js/projetos.js
+++ js/projetos.js
@@ -13,16 +13,22 @@ request.send();
 function renderizarHTMLProjetos(data){
     var bloco_projeto = "";
     for (i = 0;i < data.length; i++){
-        bloco_projeto += "<div class='col-sm-12 col-md-4 bloco_projeto'>" +
-        "<h3 class='text-center'>" + data[i].nome + "</h3>" +
-        "<img src='" + data[i].imagem +"' class='img-fluid'>" +
-        "<h4>Descrição</h4>" + 
-        "<p>" + data[i].descricao + "</p>" +
-        "<div class='btn-group btn-block'>" +
-            "<a href='" + data[i].link + "' alt='" + data[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>" +
-            "<a href='" + data[i].codigoFonte +"' alt='" + data[i].alt_codigoFonte + "' class='btn btn-secondary '>código fonte</a>" +
-        "</div>" +   
-         "</div>";
+        bloco_projeto += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
+        bloco_projeto += "<h3 class='text-center'>" + data[i].nome + "</h3>";
+        bloco_projeto += "<img src='" + data[i].imagem +"' class='img-fluid'>";
+        bloco_projeto += "<h4>Descrição</h4>"; 
+        bloco_projeto += "<p>" + data[i].descricao + "</p>";
+        bloco_projeto += "<div class='btn-group btn-block'>";
+            //caso projeto nao possua link para o projeto
+                if(data[i].link != '' || data[i].link == null || data[i].link == 0){
+                    bloco_projeto +=  "<a href='" + data[i].link + "' alt='" + data[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>";
+                }
+            //caso projeto nao possua link para o codigo fonte
+                if(data[i].codigoFonte != '' || data[i].codigoFonte == null || data[i].codigoFonte == 0){
+                    bloco_projeto += "<a href='" + data[i].codigoFonte +"' alt='" + data[i].alt_codigoFonte + "' class='btn btn-secondary '>código fonte</a>";
+                }
+             bloco_projeto += "</div>";   
+         bloco_projeto += "</div>";
     }
     area_projetos.insertAdjacentHTML('beforeend',bloco_projeto);
 }
