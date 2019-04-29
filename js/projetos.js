12f51a2669449f7c90b9a8a64a38892176629c9b
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-29 12:33:18 -0300

    add alt em link projeto e link codigo fonte

--- js/projetos.js
+++ js/projetos.js
@@ -4,13 +4,13 @@ var request = new XMLHttpRequest();
 request.open('GET','./json/projetos.json');
 request.onload = function(){
     var data = JSON.parse(request.responseText);
-     renderTodosProjetos(data);
+     renderizarHTMLProjetos(data);
     
 };
 request.send();
     
 
-function renderTodosProjetos(data){
+function renderizarHTMLProjetos(data){
     var bloco_projeto = "";
     for (i = 0;i < data.length; i++){
         bloco_projeto += "<div class='col-sm-12 col-md-4 bloco_projeto'>" +
@@ -19,8 +19,8 @@ function renderTodosProjetos(data){
         "<h4>Descrição</h4>" + 
         "<p>" + data[i].descricao + "</p>" +
         "<div class='btn-group btn-block'>" +
-            "<a href='" + data[i].link + "' class='btn btn-secondary '>ver projeto</a>" +
-            "<a href='" + data[i].codigoFonte + "' class='btn btn-secondary '>código fonte</a>" +
+            "<a href='" + data[i].link + "' alt='" + data[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>" +
+            "<a href='" + data[i].codigoFonte +"' alt='" + data[i].alt_codigoFonte + "' class='btn btn-secondary '>código fonte</a>" +
         "</div>" +   
          "</div>";
     }
