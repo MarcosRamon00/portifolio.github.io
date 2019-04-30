9ecaae7e9f9b4c5ac5f942eb19cff8eaa6165c05
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-29 23:14:14 -0300

    mudanca de nome de variaveis data -> dados , bloco_projetos div_projetos

--- js/projetos.js
+++ js/projetos.js
@@ -4,45 +4,45 @@ var area_projetos = document.getElementById("area_projetos");
 var request = new XMLHttpRequest();
 request.open('GET','./json/projetos.json');
 request.onload = function(){
-    var data = JSON.parse(request.responseText);
-     renderizarHTMLProjetos(data);    
+    var dados = JSON.parse(request.responseText);
+     renderizarHTMLProjetos(dados);    
 };
 request.send();
 
 //funcao para montar html de projetos
-function renderizarHTMLProjetos(data){
-    var bloco_projeto = "";
-    for (i = 0;i < data.length; i++){
-        bloco_projeto += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
-        bloco_projeto += "<h3 class='text-center'>" + data[i].nome + "</h3>";
-        bloco_projeto += "<img src='" + data[i].imagem +"' class='img-fluid'>";
-        bloco_projeto += "<h4>Descrição</h4>"; 
-        bloco_projeto += "<p>" + data[i].descricao + "</p>";
-        bloco_projeto += addDivBotoes(data);//add div botoes   
-        bloco_projeto += "</div>";
+function renderizarHTMLProjetos(dados){
+    var div_projeto = "";
+    for (i = 0;i < dados.length; i++){
+        div_projeto += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
+        div_projeto += "<h3 class='text-center'>" + dados[i].nome + "</h3>";
+        div_projeto += "<img src='" + dados[i].imagem +"' class='img-fluid'>";
+        div_projeto += "<h4>Descrição</h4>"; 
+        div_projeto += "<p>" + dados[i].descricao + "</p>";
+        div_projeto += addDivBotoes(dados);//add div botoes   
+        div_projeto += "</div>";
     }
-    area_projetos.insertAdjacentHTML('beforeend',bloco_projeto);
+    area_projetos.insertAdjacentHTML('beforeend',div_projeto);
 }
 
 //add div para os botoes
-function addDivBotoes(data){
+function addDivBotoes(dados){
     var div_botoes = "";
     div_botoes += "<div id='botoes' class='btn-group btn-block'>";
     //caso projeto possua link para o projeto
-    if(data[i].link != "" && data[i].link != null && data[i].link != 0){
-        div_botoes += addBotaoLink(data);
+    if(dados[i].link != "" && dados[i].link != null && dados[i].link != 0){
+        div_botoes += addBotaoLink(dados);
     }
     //caso projeto possua link para o codigo fonte
-    if(data[i].codigoFonte != "" && data[i].codigoFonte != null && data[i].codigoFonte != 0){
-        div_botoes += addBotaoCodigoFonte(data);
+    if(dados[i].codigoFonte != "" && dados[i].codigoFonte != null && dados[i].codigoFonte != 0){
+        div_botoes += addBotaoCodigoFonte(dados);
     }
     div_botoes += "</div>";
     return div_botoes;
 }
 
 //adiciona botao para link do projeto
-function addBotaoLink(data){
-    return "<a href='" + data[i].link +"' alt='" + data[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>";
+function addBotaoLink(dados){
+    return "<a href='" + dados[i].link +"' alt='" + dados[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>";
 }
 
 //adiciona botao para link do codigo fonte
