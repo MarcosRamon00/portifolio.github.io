5927cf6537c7810944782dfb9293da8d0eedc4f5
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-30 22:50:33 -0300

    mudando funcoes e variaveis para ingles

--- js/projetos.js
+++ js/projetos.js
@@ -1,76 +1,76 @@
-var area_projetos = document.getElementById("area_projetos");
+var section_projects = document.getElementById("area_projetos");
 
 //ajax executado automaticamente com o carregar da pagina
 var request = new XMLHttpRequest();
 request.open('GET','./json/projetos.json');
 request.onload = function(){
   if(this.status == 200){
-    var dados = JSON.parse(request.responseText);
-    renderizarHTMLProjetos(dados);
+    var data = JSON.parse(request.responseText);
+    renderDivProjects(data);
   }else if(this.status == 404){
-    renderizarHTMLErroNotFound();
+    renderDivErrorNotFound();
   }
   request.onerror = function(){
-    renderizarHTMLErro();
+    renderDivError();
   }      
 };
 request.send();
 
 //funcao para montar html de projetos
-function renderizarHTMLProjetos(dados){
-  var div_projeto = "";
-  for (i = 0;i < dados.length; i++){
-    div_projeto += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
-    div_projeto += "<h3 class='text-center'>" + dados[i].nome + "</h3>";
-    div_projeto += "<img src='" + dados[i].imagem +"' class='img-fluid'>";
-    div_projeto += "<h4>Descrição</h4>"; 
-    div_projeto += "<p id='descricao_projeto' class='text-justify'>" + dados[i].descricao + "</p>";
-    div_projeto += addDivBotoes(dados);//add div botoes   
-    div_projeto += "</div>";
+function renderDivProjects(data){
+  var div_project = "";
+  for (i = 0;i < data.length; i++){
+    div_project += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
+    div_project += "<h3 class='text-center'>" + data[i].nome + "</h3>";
+    div_project += "<img src='" + data[i].imagem +"' class='img-fluid'>";
+    div_project += "<h4>Descrição</h4>"; 
+    div_project += "<p id='descricao_projeto' class='text-justify'>" + data[i].descricao + "</p>";
+    div_project += addDivButtons(data);//add div botoes   
+    div_project += "</div>";
   }
-  area_projetos.insertAdjacentHTML('beforeend',div_projeto);
+  section_projects.insertAdjacentHTML('beforeend',div_project);
 }
 
 //add div para os botoes
-function addDivBotoes(dados){
-  var div_botoes = "";
-  div_botoes += "<div id='botoes' class='btn-group btn-block'>";
+function addDivButtons(data){
+  var div_buttons = "";
+  div_buttons += "<div id='botoes' class='btn-group btn-block'>";
   //caso projeto possua link para o projeto
-  if(dados[i].link != "" && dados[i].link != null && dados[i].link != 0){
-    div_botoes += addBotaoLink(dados);
+  if(data[i].link != "" && data[i].link != null && data[i].link != 0){
+    div_buttons += addButtonLink(data);
   }
   //caso projeto possua link para o codigo fonte
-  if(dados[i].codigoFonte != "" && dados[i].codigoFonte != null && dados[i].codigoFonte != 0){
-    div_botoes += addBotaoCodigoFonte(dados);
+  if(data[i].codigoFonte != "" && data[i].codigoFonte != null && data[i].codigoFonte != 0){
+    div_buttons += addButtonSourceCode(data);
   }
-  div_botoes += "</div>";
-  return div_botoes;
+  div_buttons += "</div>";
+  return div_buttons;
 }
 
 //adiciona botao para link do projeto
-function addBotaoLink(dados){
-  return "<a href='" + dados[i].link +"' alt='" + dados[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>";
+function addButtonLink(data){
+  return "<a href='" + data[i].link +"' alt='" + data[i].alt_link + "' class='btn btn-secondary '>ver projeto</a>";
 }
 
 //adiciona botao para link do codigo fonte
-function addBotaoCodigoFonte(data){
+function addButtonSourceCode(data){
   return "<a href='" + data[i].codigoFonte +"' alt='" + data[i].alt_codigoFonte + "' class='btn btn-secondary '>código fonte</a>";
 }
 
 //rederizar aviso de erro not found(404)
-function renderizarHTMLErroNotFound(){
-  var div_projeto = "";
-  div_projeto += "<div id='div_alerta_erro' class='container'>";
-  div_projeto += "<h2 class='text-center alert alert-danger'>Dados não encontrados, tente novamente mais tarde.</h2>";
-  div_projeto += "</div>"; 
-  area_projetos.insertAdjacentHTML('beforeend',div_projeto);
+function renderDivErrorNotFound(){
+  var div_project = "";
+  div_project += "<div id='div_alerta_erro' class='container'>";
+  div_project += "<h2 class='text-center alert alert-danger'>Dados não encontrados, tente novamente mais tarde.</h2>";
+  div_project += "</div>"; 
+  section_projects.insertAdjacentHTML('beforeend',div_project);
 }
 
 //renderizar aviso de erro
-function renderizarHTMLErro(){
-  var div_projeto = "";
-  div_projeto += "<div id='div_alerta_erro' class='container'>";
-  div_projeto += "<h2 class='text-center alert alert-danger'>Houve algum erro, tente novamente mais tarde.</h2>";
-  div_projeto += "</div>";
-  area_projetos.insertAdjacentHTML('beforeend',div_projeto); 
+function renderDivError(){
+  var div_project = "";
+  div_project += "<div id='div_alerta_erro' class='container'>";
+  div_project += "<h2 class='text-center alert alert-danger'>Houve algum erro, tente novamente mais tarde.</h2>";
+  div_project += "</div>";
+  section_projects.insertAdjacentHTML('beforeend',div_project); 
 }
