ad70ad88cfba65cf1218d81138efe34538f83cb0
MarcosRamon00 <marcosramon2897@gmail.com>
2019-05-05 19:13:14 -0300

    inicio da troca de javascript puro para jquery no ajax

--- README.md
+++ README.md
@@ -10,6 +10,9 @@
 - [AJAX Crash Course (Vanilla JavaScript)](https://www.youtube.com/watch?v=82hnvUYY6QA)
 - [AJAX Introduction](https://www.w3schools.com/js/js_ajax_intro.asp)
 - [mozilla - Getting Started](https://developer.mozilla.org/pt-BR/docs/Web/Guide/AJAX/Getting_Started)
+### jquery ajax
+- [jQuery - Building an Ajax GET request to retrieve JSON data](https://www.youtube.com/watch?v=BJ0tyZg2zek)
+- [jQuery Ajax error handling, show custom exception messages](https://stackoverflow.com/questions/377644/jquery-ajax-error-handling-show-custom-exception-messages)
 ### layout
 - [How to Design and Code a Portfolio Website - Part 1](https://www.youtube.com/watch?v=tMOIYCdgGw0)
 - [Bootstrap 4](https://getbootstrap.com/)
--- js/projetos.js
+++ js/projetos.js
@@ -1,100 +1,101 @@
-var section_projects = document.getElementById("area_projetos");
+var area_projects = $("#area_projetos");
+//inicio ajax
+$(document).ready(function(){
+    $.ajax({
+        url:'./json/projetos.json',
+        method:"GET",
+        dataType:"json",
+        success: function(data){
+                area_projects.append(renderDivProjects(data));
+        },
+        error:function(xhr,status,errorThrown){
+            if(xhr.status == 404){
+                area_projects.append(renderDivErrorNotFound());
+            }else{
+                area_projects.append(renderDivError());
+            }
+        }
+    });
+});
 
-//ajax executado automaticamente com o carregar da pagina
-var request = new XMLHttpRequest();
-request.open('GET','./json/projetos.json');
-request.onload = function(){
-  if(this.status == 200){
-    var data = JSON.parse(request.responseText);
-    renderDivProjects(data);
-  }else if(this.status == 404){
-    renderDivErrorNotFound();
-  }
-  request.onerror = function(){
-    renderDivError();
-  }      
-};
-request.send();
-
-//funcao para montar html de projetos
 function renderDivProjects(data){
-  let div_project = "";
-  for (i = 0;i < data.length; i++){
-    div_project += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
-    div_project += "<h3 class='text-center titulo_projeto_nome'>" + data[i].nome + "</h3>";
-    div_project += "<img src='" + data[i].imagem +"' class='img-fluid imagem_projeto'/>";
-    div_project += "<div class='bloco_texto'>";
-    div_project += "<h4 class='titulo_descricao_projeto'>Descrição</h4>"; 
-    div_project += "<p class='text-justify descricao_projeto'>" + data[i].descricao + "</p>";
-    div_project += addDivTeam(data);
-    div_project += "</div>";
-    div_project += addDivButtons(data);//add div botoes
-    div_project += "</div>";
+    let div_project = "";
+    for (i = 0;i < data.length; i++){
+      div_project += "<div class='col-sm-12 col-md-4 bloco_projeto'>";
+      div_project += "<h3 class='text-center titulo_projeto_nome'>" + data[i].nome + "</h3>";
+      div_project += "<img src='" + data[i].imagem +"' class='img-fluid imagem_projeto'/>";
+      div_project += "<div class='bloco_texto'>";
+      div_project += "<h4 class='titulo_descricao_projeto'>Descrição</h4>"; 
+      div_project += "<p class='text-justify descricao_projeto'>" + data[i].descricao + "</p>";
+      div_project += addDivTeam(data);
+      div_project += "</div>";
+      div_project += addDivButtons(data);//add div botoes
+      div_project += "</div>";
+    }
+    return div_project;
   }
-  section_projects.insertAdjacentHTML('beforeend',div_project);
-}
-//add div para equipe
+  //add div para equipe
 function addDivTeam(data){
-  let div_equipe = "";
-  if(data[i].equipe != "" && data[i].equipe != null && data[i].equipe != 0){
-    div_equipe += "<div>";
-    div_equipe += "<h4 class='titulo_equipe_projeto'>Equipe</h4>";
-    div_equipe += addLinksMembers(data);
-    div_equipe += "</div>";
+    let div_equipe = "";
+    if(data[i].equipe != "" && data[i].equipe != null && data[i].equipe != 0){
+      div_equipe += "<div>";
+      div_equipe += "<h4 class='titulo_equipe_projeto'>Equipe</h4>";
+      div_equipe += addLinksMembers(data);
+      div_equipe += "</div>";
+    }
+    
+    return div_equipe;
   }
   
-  return div_equipe;
-}
-
-//add links dos membros da equipe
-function addLinksMembers(data){
-  let linksMembros = "";
-  for (indexLink = 0;indexLink < data[i].equipe.membro.length; indexLink++){
-    linksMembros += "<span class='membro_equipe_projeto'><a class='badge badge-pill badge-custom' href='" + data[i].equipe.contato_membro[indexLink] + "' alt='link para contato com membro " + data[i].equipe.membro[indexLink]+ "'>" + data[i].equipe.membro[indexLink] + " </a></span>";
+  //add links dos membros da equipe
+  function addLinksMembers(data){
+    let linksMembros = "";
+    for (indexLink = 0;indexLink < data[i].equipe.membro.length; indexLink++){
+      linksMembros += "<span class='membro_equipe_projeto'><a class='badge badge-pill badge-custom' href='" + data[i].equipe.contato_membro[indexLink] + "' alt='link para contato com membro " + data[i].equipe.membro[indexLink]+ "'>" + data[i].equipe.membro[indexLink] + " </a></span>";
+    }
+    return linksMembros; 
   }
-  return linksMembros; 
-}
-
-//add div para os botoes
-function addDivButtons(data){
-  let div_buttons = "";
-  div_buttons += "<div class='btn-group btn-block'>";
-  //caso projeto possua link para o projeto
-  if(data[i].link.projeto != "" && data[i].link.projeto != null && data[i].link.projeto != 0){
-    div_buttons += addButtonLink(data);
+  
+  //add div para os botoes
+  function addDivButtons(data){
+    let div_buttons = "";
+    div_buttons += "<div class='btn-group btn-block'>";
+    //caso projeto possua link para o projeto
+    if(data[i].link.projeto != "" && data[i].link.projeto != null && data[i].link.projeto != 0){
+      div_buttons += addButtonLink(data);
+    }
+    //caso projeto possua link para o codigo fonte
+    if(data[i].link.codigoFonte != "" && data[i].link.codigoFonte != null && data[i].link.codigoFonte != 0){
+      div_buttons += addButtonSourceCode(data);
+    }
+    div_buttons += "</div>";
+    return div_buttons;
   }
-  //caso projeto possua link para o codigo fonte
-  if(data[i].link.codigoFonte != "" && data[i].link.codigoFonte != null && data[i].link.codigoFonte != 0){
-    div_buttons += addButtonSourceCode(data);
+  
+  //adiciona botao para link do projeto
+  function addButtonLink(data){
+    return "<a href='" + data[i].link.projeto +"' alt='" + data[i].link.alt_projeto + "' class='btn btn-custom '>ver projeto</a>";
   }
-  div_buttons += "</div>";
-  return div_buttons;
-}
-
-//adiciona botao para link do projeto
-function addButtonLink(data){
-  return "<a href='" + data[i].link.projeto +"' alt='" + data[i].link.alt_projeto + "' class='btn btn-custom '>ver projeto</a>";
-}
-
-//adiciona botao para link do codigo fonte
-function addButtonSourceCode(data){
-  return "<a href='" + data[i].link.codigoFonte +"' alt='" + data[i].link.alt_codigoFonte + "' class='btn btn-custom '>código fonte</a>";
-}
-
-//renderizar aviso de erro not found(404)
-function renderDivErrorNotFound(){
-  let div_project = "";
-  div_project += "<div id='div_alerta_erro' class='container'>";
-  div_project += "<h2 class='text-center alert alert-danger'>Dados não encontrados, tente novamente mais tarde.</h2>";
-  div_project += "</div>"; 
-  section_projects.insertAdjacentHTML('beforeend',div_project);
-}
-
-//renderizar aviso de erro
-function renderDivError(){
-  let div_project = "";
-  div_project += "<div id='div_alerta_erro' class='container'>";
-  div_project += "<h2 class='text-center alert alert-danger'>Houve algum erro, tente novamente mais tarde.</h2>";
-  div_project += "</div>";
-  section_projects.insertAdjacentHTML('beforeend',div_project); 
-}
+  
+  //adiciona botao para link do codigo fonte
+  function addButtonSourceCode(data){
+    return "<a href='" + data[i].link.codigoFonte +"' alt='" + data[i].link.alt_codigoFonte + "' class='btn btn-custom '>código fonte</a>";
+  }
+  
+  //renderizar aviso de erro not found(404)
+  function renderDivErrorNotFound(){
+    let div_project = "";
+    div_project += "<div id='div_alerta_erro' class='container'>";
+    div_project += "<h2 class='text-center alert alert-danger'>Dados não encontrados, tente novamente mais tarde.</h2>";
+    div_project += "</div>"; 
+    return div_project;
+  }
+  
+  //renderizar aviso de erro
+  function renderDivError(){
+    let div_project = "";
+    div_project += "<div id='div_alerta_erro' class='container'>";
+    div_project += "<h2 class='text-center alert alert-danger'>Houve algum erro, tente novamente mais tarde.</h2>";
+    div_project += "</div>";
+    return div_project;
+  }
